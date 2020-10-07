f6329ce39edafa6adaf062fa93488312b1af1205
braj <yuvahasin@gmail.com>
2020-10-07 16:16:59 -0400

    First Commit

---
+++ src/build/precache-manifest.727957080b4ab21c59ea0fa08df25ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8d5106291a53c4f89bffc319ff8432",
+    "url": "/index.html"
+  },
+  {
+    "revision": "571ca2d6a0304804aa4b",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "517cdc6696b7644ea93e",
+    "url": "/static/js/2.1ecaa4ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ecaa4ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571ca2d6a0304804aa4b",
+    "url": "/static/js/main.acf1ba8a.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
